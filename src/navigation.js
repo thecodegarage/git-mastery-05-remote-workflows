60a27d4002b2a684d1d5d67bb03ade2966513ab7
Bob Smith <bob@team.com>
2024-01-11 10:00:00 -0700

    Add navigation component

---
+++ src/navigation.js
@@ -0,0 +1,15 @@
+// Navigation component
+const Navigation = {
+    render() {
+        return \`
+            <nav class="main-nav">
+                <a href="/">Home</a>
+                <a href="/users">Users</a>
+                <a href="/posts">Posts</a>
+                <a href="/profile">Profile</a>
+            </nav>
+        \`;
+    }
+};
+
+module.exports = Navigation;
